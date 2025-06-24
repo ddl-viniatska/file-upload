7b42a2bb1d1188dcadedb1403733a80337e05b10
ddl-viniatska <69326214+ddl-viniatska@users.noreply.github.com>
2025-06-24 13:15:30 -0400

    Create snowflake.py

---
+++ snowflake.py
@@ -0,0 +1,11 @@
+from domino.data_sources import DataSourceClient
+
+# instantiate a client and fetch the datasource instance
+ds = DataSourceClient().get_datasource("a")
+
+# res is a simple wrapper of the query result
+res = ds.query("select * from DIABETES")
+
+# to_pandas() loads the result into a pandas dataframe
+df = res.to_pandas()
+print(df)
